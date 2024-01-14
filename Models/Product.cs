a122c910b24304d11efc7a5eed18571da2eae001
Rayni Nuñez Espino <timetopwm@gmail.com>
2024-01-13 22:30:58 -0300

    Initial commit. Already have some features covered.

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+namespace LanguageFeatures.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal? Price { get; set; }
+
+    public static Product[] GetProducts()
+    {
+        Product kayak = new Product
+        {
+            Name = "Kayak",
+            Price = 275M
+        };
+
+        Product lifeJacket = new Product
+        {
+            Name = "Life Jacket",
+            Price = 48.95M
+        };
+
+        return new Product[] { kayak, lifeJacket, null };
+    }
+}
